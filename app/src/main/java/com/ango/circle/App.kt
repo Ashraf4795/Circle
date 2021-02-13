816d9e864445e6f516c6adf2e2475c2f70762bf1
Ashraf4795 <ashraf.mohamediti4728@gmail.com>
2021-02-13 15:34:29 +0200

    setup project folder structure, refactor MainActivity directory, add koin dependencies

---
+++ app/src/main/java/com/ango/circle/App.kt
@@ -0,0 +1,11 @@
+package com.ango.circle
+
+import android.app.Application
+
+class App : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/ango/circle/views/home/MainActivity.kt
+++ app/src/main/java/com/ango/circle/views/home/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.ango.circle
+package com.ango.circle.views.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ango.circle.R
+
 //test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
