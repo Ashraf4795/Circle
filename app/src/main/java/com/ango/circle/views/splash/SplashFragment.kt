36b9f18db97f2283b4c53e12c95c6f78658d957f
Ashraf4795 <ashraf.mohamediti4728@gmail.com>
2021-02-17 21:21:50 +0200

    add fragment_splash.xml

--- app/src/main/java/com/ango/circle/views/splash/SplashFragment.kt
+++ app/src/main/java/com/ango/circle/views/splash/SplashFragment.kt
@@ -30,7 +30,7 @@ class SplashsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splashs, container, false)
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     companion object {
