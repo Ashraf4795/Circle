19cf53f6469d46e0a16da5c1aa021554de55d119
ashraf4747 <ashraf.mohamediti4728@gmail.com>
2021-06-21 20:58:58 +0200

    add default value for toast length

--- app/src/main/java/com/ango/circle/core/utils/UIHelperFunctions.kt
+++ app/src/main/java/com/ango/circle/core/utils/UIHelperFunctions.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.widget.Toast
 
 
-fun showToast(context: Context, msg: String, lengthLong: Int) {
+fun showToast(context: Context, msg: String, lengthLong: Int = Toast.LENGTH_LONG) {
     Toast.makeText(context,msg,lengthLong).show()
 }
