284d747b9c1f901fe147771b85c806c8093f70fd
ashraf4747 <ashraf.mohamediti4728@gmail.com>
2021-06-23 15:48:38 +0200

    add validator class to validate user input

---
+++ app/src/main/java/com/ango/circle/core/utils/Validator.kt
@@ -0,0 +1,18 @@
+package com.ango.circle.core.utils
+
+class Validator {
+
+    companion object {
+        fun validateEmail(email: String): Boolean {
+            return true
+        }
+
+        fun validatePassword(password: String): Boolean {
+            return true
+        }
+
+        fun validateUserName(userName: String): Boolean {
+            return true
+        }
+    }
+}
