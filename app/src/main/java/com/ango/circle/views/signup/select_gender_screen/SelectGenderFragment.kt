e2a0cbbe0536742ebc4115b9cf48286783e2829f
ashraf4747 <ashraf.mohamediti4728@gmail.com>
2021-07-28 14:38:01 +0200

    configure nav host controller in main Activity maintain gender selection and update gender button when editing add navigate to category logic when next btn get clicked add gender button shape res file
    update minSdkVersion to 21

--- app/src/main/java/com/ango/circle/views/signup/select_gender_screen/SelectGenderFragment.kt
+++ app/src/main/java/com/ango/circle/views/signup/select_gender_screen/SelectGenderFragment.kt
@@ -6,14 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.ango.circle.R
+import com.ango.circle.core.data.model.enums.Gender
+import com.ango.circle.core.utils.showToast
 import com.ango.circle.databinding.FragmentSelectGenderBinding
 import com.ango.circle.views.signup.signup_screen.SignUpViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import kotlin.math.sign
 
 class SelectGenderFragment : Fragment() {
     private val signupViewModel by sharedViewModel<SignUpViewModel>()
-    private lateinit var selectGenderBinding:FragmentSelectGenderBinding
+    private var isMaleClicked = false
+    private var isFemaleClicked = false
+    private lateinit var selectGenderBinding: FragmentSelectGenderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,13 +36,70 @@ class SelectGenderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        selectGenderBinding = FragmentSelectGenderBinding.inflate(inflater,container,false)
+        selectGenderBinding = FragmentSelectGenderBinding.inflate(inflater, container, false)
         return selectGenderBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("User","user is ${signupViewModel.user}")
+        updateGenderButtons()
+        setGenderButtonsListener()
+        setSelectUserProfileImageListener()
+        //Log.d("User","user is ${signupViewModel.user}")
+    }
+
+    private fun setSelectUserProfileImageListener() {
+        selectGenderBinding.userImgId.setOnClickListener{
+            showToast(requireContext(),"user profile image")
+        }
+    }
+
+    private fun setGenderButtonsListener() {
+        selectGenderBinding.maleBtnId.setOnClickListener {
+            signupViewModel.user.userGender = Gender.MALE
+            updateGenderButtons()
+
+        }
+        selectGenderBinding.femaleBtnId.setOnClickListener {
+            signupViewModel.user.userGender = Gender.FEMALE
+            updateGenderButtons()
+        }
+        selectGenderBinding.nextBtnId.setOnClickListener{
+            findNavController().navigate(R.id.action_selectGenderFragment_to_categoryFragment2)
+        }
+    }
+
+    private fun updateGenderButtons() {
+        setGenderFlagsValue()
+        with(selectGenderBinding) {
+            if (isMaleClicked) {
+                updateButtonClickState(maleBtnId, R.drawable.gender_button_selected_shap)
+            } else {
+                updateButtonClickState(maleBtnId, R.drawable.gender_button_shape)
+            }
+            if (isFemaleClicked) {
+                updateButtonClickState(femaleBtnId, R.drawable.gender_button_selected_shap)
+            } else {
+                updateButtonClickState(femaleBtnId, R.drawable.gender_button_shape)
+            }
+        }
+    }
+
+    private fun setGenderFlagsValue() {
+        signupViewModel.user.let { user ->
+            val isMale = signupViewModel.user.userGender == Gender.MALE
+            isMaleClicked = isMale
+            isFemaleClicked = !isMale
+        }
+
+
+    }
+
+    private fun updateButtonClickState(
+        layout: LinearLayout,
+        layoutBg: Int,
+    ) {
+        layout.setBackground(ContextCompat.getDrawable(requireContext(), layoutBg))
     }
 
 }
